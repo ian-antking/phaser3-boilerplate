4e56b6b3e9289d2ad3af436149638a2ca0a127b1
Ian King <ian.antking@gmail.com>
2022-08-14 10:12:16 +0100

    add blank project

---
+++ src/config/index.js
@@ -0,0 +1,10 @@
+import { AUTO } from 'phaser'
+
+import 'phaser'
+
+export default {
+  type: AUTO,
+  parent: 'phaser-example',
+  width: 900,
+  height: 600,
+}
---
+++ src/scenes/game.js
@@ -0,0 +1,7 @@
+import Phaser from 'phaser'
+
+export default class GameScene extends Phaser.Scene {
+  constructor() {
+    super('Game')
+  }
+}
